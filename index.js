e6ae53290e82aa0b6bc2a09e4034575e00723463
danpatoneil <danpatoneil@gmail.com>
2024-04-29 15:38:42 -0400

    Altered Shape class, updated tests
    Added a text color attribute to both the index.js inquirer and the shape class and the 3 classes that extend it. Altered tests to include this.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Circle = require('./lib/circle.js');
 const CSS_COLORS = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
 const CSS_Hexcode_regex = new RegExp('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$');
 
-
+//text, text color, shape, shape color
 
 inquirer
     .prompt([
@@ -20,6 +20,16 @@ inquirer
             validate: (input) => {//validate checks if the provided function returns true and if not allows the user to reattempt until it does
                 if(input.length<=3&&input.length>=1) return true;//this validate simply checks if the input is less than 3 characters
                 else return 'Your text was either less than 1 or more than 3 characters, please enter only 1-3 characters';
+            },
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Please enter the color you want the text on your logo to be in either a valid CSS color or in hexadecimal lead by a "#"',
+            validate: (input) => {
+                if(CSS_COLORS.includes(input)) return true;//this input succeeds if the user enters something that was a valid CSS color by checking against a list of all valid CSS colors
+                else if(CSS_Hexcode_regex.test(input)) return true;// this input succeeds if the input is a valid hexadecimal input for a color
+                else return 'Your input was not either a valid CSS color or a valid CSS color hexcode';
             },
         },
         {
@@ -43,21 +53,21 @@ inquirer
         //switch statement figures out which kind of new object to make, constructs it and writes the render function to file
         switch (data.shape) {
             case 'Circle':
-                const circle = new Circle(data.text, data.color)
+                const circle = new Circle(data.text, data.textColor, data.color)
                 fs.writeFile('logo.svg', circle.render(), (err) =>
                     err ? console.error(err) : console.log('Generated logo.svg')
                 );
                 break;
 
             case 'Triangle':
-                const triangle = new Triangle(data.text, data.color)
+                const triangle = new Triangle(data.text, data.textColor, data.color)
                 fs.writeFile('logo.svg', triangle.render(), (err) =>
                     err ? console.error(err) : console.log('Generated logo.svg')
                 );
                 break;
 
             case 'Square':
-                const square = new Square(data.text, data.color)
+                const square = new Square(data.text, data.textColor, data.color)
                 fs.writeFile('logo.svg', square.render(), (err) =>
                     err ? console.error(err) : console.log('Generated logo.svg')
                 );
